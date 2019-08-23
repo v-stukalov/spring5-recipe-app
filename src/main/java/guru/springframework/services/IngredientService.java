7b771d5e8259fae8af78c5b066a519ac826a0ba7
vitaliistukalov <vitaliy.stukalov@gmail.com>
2019-08-23 01:16:55 -0500

    update existing ingredient

--- src/main/java/guru/springframework/services/IngredientService.java
+++ src/main/java/guru/springframework/services/IngredientService.java
@@ -1,8 +1,12 @@
 package guru.springframework.services;
 
 import guru.springframework.commands.IngredientCommand;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface IngredientService {
 
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+
+    @Transactional
+    IngredientCommand saveIngredientCommand(IngredientCommand command);
 }
